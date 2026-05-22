2936863ce7dc1f5033018e666e3a73a3fc8f3ae6
jamal <jamalderea@gmail.com>
2026-05-22 19:46:53 +0200

    Initial commit - routing, navbar and Bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Books } from './components/books/books';
+import { Quotes } from './components/quotes/quotes';
+import { Login } from './components/login/login';
+import { Register } from './components/register/register';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'books', pathMatch: 'full' },
+  { path: 'books', component: Books },
+  { path: 'quotes', component: Quotes },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register }
+];
+
+
